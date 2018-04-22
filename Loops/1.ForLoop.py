4ee86b5f69b520cebd1a023c02198434b64a38b2
chavarera <chavare.ravi123@gmail.com>
2018-04-21 20:59:22 -0700

    Simple For loop

---
+++ Loops/1.ForLoop.py
@@ -0,0 +1,31 @@
+# for loop is used for looping purpose
+
+# for loop contains three parameter
+# 1.starting number
+# 2.Stop number+1
+# 3.difference or step number(is optional if not given then it will increament by 1)
+
+#to write out for logic
+#for variable in (list,varaible,tuple,dictionaries,string or any iterable object here):
+#     //remaing code for for
+
+#For Example print 1 to 10 numbers
+
+for i in range(1,11):
+    print(i)
+
+
+#in above example 1 is starting number and 11 is ending number means print number between 1 and 11 is 1,2,3,4,5,6,7,8,9,10
+
+
+#result of code
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+# 10
