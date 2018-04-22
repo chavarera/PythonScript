aad2fcd719f1179657fbc05b9518bb3907e08493
chavarera <chavare.ravi123@gmail.com>
2018-04-21 21:12:59 -0700

    basic of while loop

---
+++ Loops/4.WhileLoop.py
@@ -0,0 +1,16 @@
+#simlar to for loop while loop is used for iterbale object
+#while loop run block of code untill condtion is true when condition is false it will terminate execution of code block
+
+#Syntax
+# while condition:
+#    code of while block
+
+
+# simple example to print 1 to 10 numbers using while loop
+num=0
+while num<11:
+    print(num)
+    num=num+1
+
+
+#while block run until num is less than 11 means 0 to 10 numbers printed
