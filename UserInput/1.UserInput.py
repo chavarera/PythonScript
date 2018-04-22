98436f4a82a28798f07322a342f04c658e686671
chavarera <chavare.ravi123@gmail.com>
2018-04-21 22:36:10 -0700

    basic of user input

---
+++ UserInput/1.UserInput.py
@@ -0,0 +1,11 @@
+#For Getting user input we used keyword input() function
+#for example if you want to ask user name then
+username=input('Enter user name:')  #this will ask user on terminal on same line
+
+print('Enter Username:')
+uname=input()   #this will ask input on new line
+
+
+
+print(username)
+print(uname)
