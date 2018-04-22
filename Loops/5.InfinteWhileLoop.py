18b24e6755313bddd811011086115c0878a5ef21
chavarera <chavare.ravi123@gmail.com>
2018-04-21 21:13:18 -0700

    create unstoppable loop

---
+++ Loops/5.InfinteWhileLoop.py
@@ -0,0 +1,16 @@
+# to create a infinte loop in condion give such condtion which is always true then that block of code infintely runs
+
+#Example 1
+i=1
+while(i!=10):
+    i=i+2
+    print(i)
+
+#here we provide condition i!=10 which is always give output as true then this will run infinte times
+
+#Example 2
+
+# while True:
+#     print('HI')
+
+#in this example write True in condition
