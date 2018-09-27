b78131dc2de0e17e1fd89c76a345376daacd955c
Ravishankar Chavare <33047641+chavarera@users.noreply.github.com>
2018-09-27 11:34:05 +0530

    Update study.py

--- study.py
+++ study.py
@@ -3,7 +3,7 @@ features
 python 2 and python 3
 installation of python and environment 
 basic variable datatypes,indentation,comments
-function
+function and lambda
 swap two numbers
 strings
 user input
