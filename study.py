b8d4a554220322df3b50ac4ec5418fc33993bc93
Ravishankar Chavare <33047641+chavarera@users.noreply.github.com>
2018-09-27 10:58:12 +0530

    Create study.py

---
+++ study.py
@@ -0,0 +1,13 @@
+introduction
+features
+python 2 and python 3
+installation of python and environment 
+basic variable datatypes,indentation,comments
+function
+strings
+user input
+list
+loop
+modules(os)
+
+
