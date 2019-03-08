5a141c3adec7a569b95c1edd1385a5367cdb5765
Ravishankar Chavare <33047641+chavarera@users.noreply.github.com>
2019-03-09 00:03:57 +0530

    Create 1.IntroductionOfPandas.py

---
+++ MachineLearning/Pandas/1.IntroductionOfPandas.py
@@ -0,0 +1,83 @@
+
+##--------------------------------
+##Created By:Ravishankar Chavare
+##version:python 3.7
+##Date:24/12/18
+##File_des:Introduction of Pandas
+##--------------------------------
+
+##Pandas:-
+
+##**********************************************************************************************************************************
+#                                                ----About----
+##**********************************************************************************************************************************
+'''
+-pandas is a Python package providing fast, flexible, and expressive data structures designed to make working with “relational” or
+“labeled” data both easy and intuitive.
+It aims to be the fundamental high-level building block for doing practical, real world data analysis in Python
+
+-pandas is a software library written for the Python programming language for data manipulation and analysis.
+In particular, it offers data structures and operations for manipulating numerical tables and time series.
+
+-Tools for reading and writing data between in-memory data structures and different formats: CSV and text files,
+Microsoft Excel, SQL databases, and the fast HDF5 format;
+'''
+
+##**********************************************************************************************************************************
+#                                          ----Why Pandas---
+##**********************************************************************************************************************************
+'''
+-Pandas is arguably the most important Python package for data science
+-pandas is an opensource
+-Its can work with data from different type of data sources like,csv,excel,dat,text and much more
+-It gives productivity and performance according to others
+-It allows fast analasys and data cleaning preparation
+-Some Builtin Visualization Features
+-High performance merging and joining of data sets;
+-optimized for performance
+'''
+
+
+##**********************************************************************************************************************************
+#                                 ---Benifits of using pandas---
+##**********************************************************************************************************************************
+'''
+-Easy handling of missing data (represented as NaN) in floating point as well as non-floating point data
+-Size mutability: columns can be inserted and deleted from DataFrame and higher dimensional objects
+-Automatic and explicit data alignment: objects can be explicitly aligned to a set of labels, or
+the user can simply ignore the labels and let Series, DataFrame, etc. automatically align the data for you in computations
+-Powerful, flexible group by functionality to perform split-apply-combine operations on data sets, for both aggregating and transforming data
+-Make it easy to convert ragged, differently-indexed data in other Python and NumPy data structures into DataFrame objects
+'''
+
+
+##**********************************************************************************************************************************
+#                                ----Insallation and Setup----
+##**********************************************************************************************************************************
+'''
+You can simply install pandas using command line on terminal
+in windows just open your command prompt and type:
+
+pip install pandas
+
+
+press enter key and wait unitll pandas to be install on your machine
+
+For Upgrading pandas using pip
+python3 -m pip install --upgrade pandas
+'''
+##**********************************************************************************************************************************
+#                                  ---Import Use in Pandas---
+##**********************************************************************************************************************************
+'''
+import pandas as pd
+import numpy as np
+'''
+
+import pandas as pd
+import numpy as np
+
+
+
+
+
