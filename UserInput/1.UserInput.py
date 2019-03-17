6099e947a532aa73e913d39d927cdc3d821260e3
Ravishankar Chavare <33047641+chavarera@users.noreply.github.com>
2019-03-17 16:48:45 +0530

    Update 1.UserInput.py

--- UserInput/1.UserInput.py
+++ UserInput/1.UserInput.py
@@ -1,5 +1,7 @@
 #For Getting user input we used keyword input() function
 #for example if you want to ask user name then
+#in python 2. version we need to get number using input() and strings using raw_input()
+#Now in Python 3. version input() can handle string and number both
 username=input('Enter user name:')  #this will ask user on terminal on same line
 
 print('Enter Username:')
